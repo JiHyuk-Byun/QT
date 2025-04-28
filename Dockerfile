73612a0d24ae812058f415e612803f27e081fb91
join <choin1116@gmail.com>
2025-04-28 20:55:39 +0900

    [CIPRUN v0000] Commit for version v0000 by CIPRUN

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+
+ENV TORCH_CUDA_ARCH_LIST="6.1;7.0;7.5;8.0;8.6"
+ENV LANG=C.UTF-8
+ENV LC_ALL=C.UTF-8
+
+RUN apt update
+RUN apt install software-properties-common libopencv-dev git -y
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt install python3.9 python3-pip python3.9-dev -y
+RUN rm /usr/bin/python3 && ln -s /usr/bin/python3.9 /usr/bin/python3
+RUN pip3 install --upgrade pip
+RUN pip3 install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118
+RUN pip3 install ninja setuptools==68.0.0
+
+RUN mkdir /root/external
+ADD requirements.txt /root/external/
+RUN pip3 install -r /root/external/requirements.txt
+
+WORKDIR /workspace
--- train.py
+++ train.py
@@ -7,14 +7,14 @@ import lightning.pytorch as pl
 
 import engine
 
-
 # Train + evaluation for every evaluation_freq
 
 parser = ArgumentParser()
 parser.add_argument('--config', '-c', type=str, required=True)
 parser.add_argument('--name', '-n', type=str, help='Name of the experiment')
 parser.add_argument('--debug', '-d', action='store_true', default=False, help='debug mode (for sanity check)')
-parser.add_argument('--gpus', '-g', default='-1', help='GPU to use (num. GPU or gpu ids, follow pytorch-lightning convention). e.g., "-1" (all), "2" (2 GPU), "0,1" (GPU id 0, 1), "[0]" (GPU id 0)')
+parser.add_argument('--gpus', '-g', default='-1',
+                    help='GPU to use (num. GPU or gpu ids, follow pytorch-lightning convention). e.g., "-1" (all), "2" (2 GPU), "0,1" (GPU id 0, 1), "[0]" (GPU id 0)')
 args = parser.parse_args()
 
 
@@ -24,19 +24,19 @@ def main():
     pl.seed_everything(cfg.get('seed', 123456))
     if args.debug:
         args.name = 'debugging'
-    
+
     # Step 2. Create output directory and save current configuration.
     engine.create_experiment_context(cfg.get('output_dir', None), args.name)
     with open(osp.join(engine.to_experiment_dir('config.yaml')), 'w') as f:
         f.write(OmegaConf.to_yaml(cfg, resolve=True))
 
-    
     model = engine.instantiate(cfg.model)
     dm = engine.instantiate(cfg.data)
     solver = engine.instantiate(cfg.solver, dm=dm, model=model)
 
     trainer: pl.Trainer = engine.prepare_trainer(cfg, gpus=args.gpus, debug=args.debug)
     trainer.fit(model=solver, datamodule=dm)
+
 
 if __name__ == '__main__':
     main()
