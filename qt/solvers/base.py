c5b9ce930ee921b4d4f8a0dd726052f2f5058c61
JiHyuk-Byun <quswlgur123@yonsei.ac.kr>
2025-05-03 10:16:57 +0900

    [CIPRUN v0039] Commit for version v0039 by CIPRUN

--- qt/solvers/base.py
+++ qt/solvers/base.py
@@ -62,7 +62,10 @@ class BaseSolver(LightningModule):
         
         state_dict = checkpoint['state_dict']
         self.load_state_dict(state_dict, strict=False)
-        self.checkpoint_epoch = checkpoint['epoch']
+        self.current_epoch = checkpoint['epoch']
+        self.global_step = checkpoint['global_step']
+
+        #self.checkpoint_epoch = checkpoint['epoch']
 
     def reset_metrics(self):
         if self._metrics is None:
