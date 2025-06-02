cd19aa917c523e5727c2446424e776cd85c89b8c
JiHyuk-Byun <quswlgur123@yonsei.ac.kr>
2025-06-03 01:06:11 +0900

    [CIPRUN v0098] Commit for version v0098 by CIPRUN

--- qt/solvers/evaluation_solver.py
+++ qt/solvers/evaluation_solver.py
@@ -2,6 +2,7 @@ import os
 from os import path as osp
 from typing import List
 import csv
+import json
 
 import torch
 import numpy as np
@@ -9,6 +10,8 @@ import numpy as np
 import engine
 from .base import BaseSolver
 from qt.data import QA3DBaseDataModule
+
+GOBJAVERSE_INDEX_PATH = 'datasets/json_files/objaverse_index_to_gobjaverse.json'
 
 
 class EvaluationSolver(BaseSolver):
@@ -22,6 +25,12 @@ class EvaluationSolver(BaseSolver):
         self.output_dir = engine.to_experiment_dir('outputs', self.dm.name)
         os.makedirs(self.output_dir, exist_ok=True)
 
+        self.index_map = None
+        if self.dm.name == 'objaverse':
+            with open(GOBJAVERSE_INDEX_PATH, 'r') as f:
+                index_map = json.load(f)
+                self.index_map = {key.replace('.glb', ''): val for key, val in index_map.items()}
+
     def validation_step(self, batch, batch_idx):
 
         outputs = self.solver(batch)
@@ -32,7 +41,7 @@ class EvaluationSolver(BaseSolver):
         #     print('-'*40)
         #     print(f'key: {k}')
         #     print(f'value:{v}')
-            
+
         #     if isinstance(v, torch.Tensor):
         #         print(f'shape: {v.shape}')
         #         print(f'scale: {v.min()}~{v.max()}')
@@ -41,26 +50,23 @@ class EvaluationSolver(BaseSolver):
         #         print(f'scale: {min(v)}~{max(v)}')
         #     else:
         #         print(f'TypeError: {v.type}')
-                
 
         ids = batch['id']
         labels = batch['mos'].view_as(preds)
         labels = labels.detach()
-        
-        
+
         self._all_preds.append(preds)
         self._all_labels.append(labels)
         self._all_ids.append(ids)
-#        _, _, preds_normalized = self._logistic_4_fitting(preds)
-#        labels_normalized = self._logistic_4_fitting(labels)
-        # print(f'preds_norm: {preds_normalized[:20]}')
-        # print(f'MOS_norm: {labels[:20]}')
-       
 
+    #        _, _, preds_normalized = self._logistic_4_fitting(preds)
+    #        labels_normalized = self._logistic_4_fitting(labels)
+    # print(f'preds_norm: {preds_normalized[:20]}')
+    # print(f'MOS_norm: {labels[:20]}')
 
     def on_validation_epoch_end(self):
-        
-        #for i, crit in enumerate(self.criterion):
+
+        # for i, crit in enumerate(self.criterion):
 
         preds_local = torch.cat(self._all_preds, dim=0)
         labels_local = torch.cat(self._all_labels, dim=0)
@@ -79,47 +85,71 @@ class EvaluationSolver(BaseSolver):
         ids_all = [id for sub in ids_all for id in sub]
 
         self._save_csv(ids_all, preds_all, labels_all)
-        
+
         scores_no_fitted = dict()
         scores_fitted = dict()
+        score_pairs_list = []
+
         for i, crit in enumerate(self.criterion):
             pred = preds_all[:, i].numpy()
             gt = labels_all[:, i].numpy()
-            print('pred: ', pred)
-            print('labels:', gt)
+            print(f'--- criterion: {crit} ---')
+            for p, l in zip(pred, gt):
+                print(f'pred: {p}, gt: {l}')
+
+            score_pairs_list.append((pred.tolist(), gt.tolist()))
 
             pred_norm = self._min_max_normalize(pred)
             gt_norm = self._min_max_normalize(gt)
             pred_norm_t = torch.from_numpy(pred_norm)
             gt_norm_t = torch.from_numpy(gt_norm)
-            
+
             metrics_no_fitted = self._evaluate_metrics(pred_norm_t, gt_norm_t)
             scores_no_fitted[crit] = metrics_no_fitted
 
             _, _, pred_fitted = self._logistic_4_fitting(pred, gt)
             preds_t = torch.from_numpy(pred_fitted)
             gt_t = torch.from_numpy(gt)
-            
+
             metrics_fitted = self._evaluate_metrics(preds_t, gt_t)
             scores_fitted[crit] = metrics_fitted
-        
+
+        ## saving results for visualization
+        self._save_result_json(ids_all, self.criterion, score_pairs_list)
+
         result_str_no_fitted = ''
         result_str_fitted = ''
         for c in self.criterion:
             result_str_no_fitted += f"[{c}] PLCC: {scores_no_fitted[c]['plcc']}, SROCC: {scores_no_fitted[c]['srocc']}, KROCC: {scores_no_fitted[c]['krocc']}, RMSE: {scores_no_fitted[c]['rmse']}\n"
             result_str_fitted += f"[{c}] PLCC: {scores_fitted[c]['plcc']}, SROCC: {scores_fitted[c]['srocc']}, KROCC: {scores_fitted[c]['krocc']}, RMSE: {scores_fitted[c]['rmse']}\n"
-        
+
         result = f'---No fitted Result---\n {result_str_no_fitted}' + '\n' + f'---fitted Result---\n {result_str_fitted}'
         print(result)
         out_path = osp.join(self.output_dir, f'eval_results_{self.criterion}.txt')
-        
+
         with open(out_path, 'w') as f:
             f.write(result)
-            
-    def _save_csv(self, ids: List[str], preds: torch.Tensor, scores: torch.Tensor):
 
-        eval_result = {'id': ids}                          
-        for i, crit in enumerate(self.criterion):          
+    def _save_result_json(self, ids: List[str], criterion: List[str], score_pairs_list):
+        ## convert objaverse indices to gid
+        if self.index_map is not None:
+            ids = [x.replace('.npy', '') for x in ids]
+            ids = [self.index_map[x] for x in ids]
+
+        results = {
+            'dataset': self.dm.name,
+            'ids': ids,
+            'criterion': criterion,
+            'score_pairs': score_pairs_list,
+        }
+
+        save_path = os.path.join(self.output_dir, f'eval_results.json')
+        with open(save_path, 'w') as f:
+            json.dump(results, f, indent=4)
+
+    def _save_csv(self, ids: List[str], preds: torch.Tensor, scores: torch.Tensor):
+        eval_result = {'id': ids}
+        for i, crit in enumerate(self.criterion):
             eval_result[crit] = preds[:, i].tolist()
 
         eval_result['gt(preference)'] = (
@@ -128,11 +158,10 @@ class EvaluationSolver(BaseSolver):
 
         outpath = osp.join(self.output_dir, "eval_scores.csv")
 
-        header = list(eval_result.keys())                  # ['id', crit1, crit2, ..., 'gt(preference)']
+        header = list(eval_result.keys())  # ['id', crit1, crit2, ..., 'gt(preference)']
         with open(outpath, "w", newline="", encoding="utf-8") as f:
             writer = csv.DictWriter(f, fieldnames=header)
             writer.writeheader()
 
             for row_vals in zip(*eval_result.values()):
                 writer.writerow(dict(zip(header, row_vals)))
-
